b73a0c414c1500a6c1a7d7b9239ce5bcc88fa8e9
JasmnC <chen.jasm@northeastern.edu>
2022-11-19 12:35:04 -0500

    origin

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Border color="blue">
+      <h2 class="title">
+        WELCOME!
+      </h2>
+      <button>show IP</button>
+      <button>show latency</button>
+  </Border>
+  );
+}
+
+function Border(props){
+  return (
+    <div className={'boarder'+props.color}>
+      {props.children}
     </div>
   );
 }
